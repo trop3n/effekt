1a63869700fa5bab960cd79c4ba719a0f0ac29d1
trop3n <jasondkimm@proton.me>
2026-03-05 10:23:34 -0600

    fixed upside down load error

--- src/gl/TextureLoader.ts
+++ src/gl/TextureLoader.ts
@@ -4,6 +4,7 @@ export function uploadImageTexture(
 ): WebGLTexture {
   const tex = gl.createTexture()!;
   gl.bindTexture(gl.TEXTURE_2D, tex);
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA8, gl.RGBA, gl.UNSIGNED_BYTE, image);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
